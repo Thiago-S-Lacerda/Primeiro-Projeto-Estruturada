fa00019ae9b5390f9ffc868d3bee97cf8ad9581c
Thiago Santos de Lacerda <thiagostslacerda@gmail.com>
2026-06-20 11:03:24 -0300

    Adicionado função temVencedor; Função marcaJogada melhorada

--- src/exemplo.c
+++ src/exemplo.c
@@ -5,8 +5,9 @@
 
 int main() {
 	setlocale(LC_ALL, "Portuguese");
-	int matrizEscolha[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
-	char matrizResposta[3][3] = {{'_','_','_'}, {'_','_','_'}, {' ',' ',' '}};
+	int posicoes[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+	char tabuleiro[3][3] = {{'_','_','_'}, {'_','_','_'}, {' ',' ',' '}};
+	int resultado;
 	
 	printf("Exemplo das posições:\n");
 	printf("\t_1_|_2_|_3_\n");
@@ -16,8 +17,19 @@ int main() {
 	printf("\033[2J\033[H");
 
 	do {
-		desenha(matrizEscolha, matrizResposta);
-        marcaJogada(matrizEscolha, matrizResposta, "Thiago", "Caio");
+		desenha(posicoes, tabuleiro);
+        marcaJogada(posicoes, tabuleiro, "Thiago", "Caio");
+
+		resultado = temVencedor(posicoes, "Thiago", "Caio");
+		if (resultado == 2) {
+			desenha(posicoes, tabuleiro);
+			printf("%s venceu!!", "Caio");
+			break;
+		} else if (resultado == 1) {
+			desenha(posicoes, tabuleiro);
+			printf("%s venceu!!", "Thiago");
+			break;
+		}
 	} while(1);
 	
 	return 0;
--- src/funcoes_thiago.c
+++ src/funcoes_thiago.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 #include "funcoes_thiago.h"
 
@@ -13,7 +14,7 @@ void desenha(int matrizJogada[][3], char tabuleiro[][3]) {
 
 void marcaJogada(int matrizJogada[][3], char tabuleiro[][3], char jogador1[], char jogador2[]) {
     setlocale(LC_ALL, "Portuguese");
-    int posicao, i, j;
+    int posicao, i, j, posicaoInvalida = 1;
 	
     static int turno_jogador = 2;
 
@@ -26,10 +27,14 @@ void marcaJogada(int matrizJogada[][3], char tabuleiro[][3], char jogador1[], ch
                 if (matrizJogada[i][j] == posicao) {
                     matrizJogada[i][j] = 100;
                     tabuleiro[i][j] = 'O';
+                    posicaoInvalida = 0;
                 }
             }
         }
-        turno_jogador = 2;
+        if (posicaoInvalida == 0) {
+            turno_jogador = 2;
+        }
+
     } else if (turno_jogador == 2) {
         printf("Vez de %s\n", jogador2);
         printf("Digite a posição que você quer jogar: ");
@@ -39,9 +44,60 @@ void marcaJogada(int matrizJogada[][3], char tabuleiro[][3], char jogador1[], ch
                 if (matrizJogada[i][j] == posicao) {
                     matrizJogada[i][j] = 400;
                     tabuleiro[i][j] = 'X';
+                    posicaoInvalida = 0;
                 }
             }
         }
-        turno_jogador = 1;
+        if (posicaoInvalida == 0) {
+            turno_jogador = 1;
+        }
     }
+
+    if ((posicao > 0 && posicao < 10) && posicaoInvalida == 1) {
+        printf("Essa posição já está ocupada! Tente novamente\n");
+        system("pause");
+        desenha(matrizJogada, tabuleiro);
+        marcaJogada(matrizJogada, tabuleiro, jogador1, jogador2);
+    } else if (posicao < 1 || posicao > 9) {
+        printf("Essa posição não existe! As posições possíveis são apenas de 0 a 9\n");
+        system("pause");
+        desenha(matrizJogada, tabuleiro);
+        marcaJogada(matrizJogada, tabuleiro, jogador1, jogador2);
+    }
+}
+
+int temVencedor(int matrizJogada[][3], char jogador1[], char jogador2[]) {
+    // Verificação das linhas e colunas
+    int i, j, somaLinhas, somaColunas;
+    for (i = 0; i < 3; i++) {
+        somaLinhas = 0;
+        somaColunas = 0;
+        for (j = 0; j < 3; j++) {
+            somaLinhas += matrizJogada[i][j];
+            somaColunas += matrizJogada[j][i];
+            if (somaLinhas == 300 || somaColunas == 300) {
+                printf("%s venceu!!", jogador1);
+                return 1;
+            } else if (somaLinhas == 1200 || somaColunas == 1200) {
+                printf("%s venceu!!", jogador2);
+                return 2;
+            }
+        }
+	}
+
+    // Verificação das diagonais
+    int somaDiagonalP = 0, somaDiagonalS = 0;
+    for (i = 0; i < 3; i++) {
+        somaDiagonalP += matrizJogada[i][i];
+        somaDiagonalS += matrizJogada[i][2-i];
+	}
+    if (somaDiagonalP == 300 || somaDiagonalS == 300) {
+                printf("%s venceu!!", jogador1);
+                return 1;
+            } else if (somaDiagonalP == 1200 || somaDiagonalS == 1200) {
+                printf("%s venceu!!", jogador2);
+                return 2;
+            }
+
+    return 0;
 }
